f65955acf70f4f2f665ec4a8b61ef6712ec26b86
kpoe03 <52971991+kpoe03@users.noreply.github.com>
2019-09-08 21:22:19 -0400

    first commit

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -10,7 +10,7 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
     // firstItem passes the first item of the given array to the callback function.
   }
 
-  // SOLUTION:
+  // SOLUTION: //
 
   function firstItem(arr, cb) {
     return cb(arr[0]);
@@ -41,19 +41,34 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
-}
+  cb(arr.length);
+};
+getLength(items, length => {
+  console.log(`The length of the array is ${length}.`);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
-}
+  cb(arr[arr.length - 1]);
+};
+last(items, lastItem => {
+  console.log(`The last item in the array is ${lastItem}.`);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-}
-
+  cb( x + y );
+};
+sumNums(5, 10, sum => {
+  console.log(`The sum is ${sum}.`);
+});
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
-}
+  cb( x * y );
+};
+multiplyNums(5, 10, product => {
+  console.log(`The product is ${product}.`);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
